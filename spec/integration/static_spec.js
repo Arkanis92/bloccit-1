6c0f234a0bce76d7b7e436c38b4b25688f9458a8
Zoe Woodward <zoefwoodward@gmail.com>
2018-05-29 18:53:05 -0400

    About Us Added to Nav

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,11 +3,27 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
+
     describe("GET /", () => {
-        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+            
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(body).toContain("Welcome to Bloccit");
+
+                done();
+            });
+        });
+    });
+
+    describe("GET /about", () => {
+
+        it("should return status code 200 and have 'About us' in the body of the response", (done) => {
+
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain("About Us");
 
                 done();
             });
