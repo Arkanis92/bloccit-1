4dea4b11302a204fcb15652ed2e65b2e50bb1583
Zoe Woodward <zoefwoodward@gmail.com>
2018-06-13 21:45:03 -0400

    Flair Feature

--- src/routes/flairs.js
+++ src/routes/flairs.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const flairController = require("../controllers/flairController")
 
-router.get("/flairs/new", flairController.new);
+router.get("/posts/:postId/flairs/new", flairController.new);
+
+router.post("/posts/:postId/flairs/create", flairController.create);
+
+router.get("/posts/:postId/flairs/:id", flairController.show);
+
+router.post("/posts/:postId/flairs/:id/destroy", flairController.destroy);
+
+router.get("/posts/:postId/flairs/:id/edit", flairController.edit);
+
+router.post("/posts/:postId/flairs/:id/update", flairController.update);
 
 module.exports = router;
