3e2266e81c119059e1f2bbcdf8edb5956f1c820b
Zoe Woodward <zoefwoodward@gmail.com>
2018-06-15 15:52:03 -0400

    Added Authentication

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,13 +1,15 @@
 module.exports = {
     init(app){
         const staticRoutes = require("../routes/static");
-        const postRoutes = require("../routes/posts");
         const topicRoutes = require("../routes/topics");
+        const postRoutes = require("../routes/posts");
+        const userRoutes = require("../routes/users");
         
        
         app.use(staticRoutes);
-        app.use(postRoutes);
         app.use(topicRoutes);
+        app.use(postRoutes);
+        app.use(userRoutes);
         
     }
 }
