5570d7657d698a66dced826b4e13521ec6559b2f
Zoe Woodward <zoefwoodward@gmail.com>
2018-06-29 15:39:37 -0400

    Favoriting Posts, fixed edit and delete functions

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,15 +1,14 @@
-const bcrypt = require("bcryptjs");
 const User = require("./models").User;
 const Post = require("./models").Post;
 const Comment = require("./models").Comment;
+const Favorite = require("./models").Favorite;
+const bcrypt = require("bcryptjs");
 
 module.exports = {
 
   createUser(newUser, callback){
-
     const salt = bcrypt.genSaltSync();
     const hashedPassword = bcrypt.hashSync(newUser.password, salt);
-
     return User.create({
       email: newUser.email,
       password: hashedPassword
@@ -23,32 +22,38 @@ module.exports = {
   },
 
   getUser(id, callback){
- 
-       let result = {};
-       User.findById(id)
-       .then((user) => {
-         if(!user) {
-           callback(404);
-         } else {
+    let result = {};
+    User.findById(id)
+    .then((user) => {
+      if(!user) {
+        callback(404);
+      } else {
+        result["user"] = user;
 
-           result["user"] = user;
+        Post.scope({method: ["lastFiveFor", id]}).all()
+        .then((posts) => {
+          result["posts"] = posts;
 
-           Post.scope({method: ["lastFiveFor", id]}).all()
-           .then((posts) => {
+          Comment.scope({method: ["lastFiveFor", id]}).all()
+          .then((comments) => {
+            result["comments"] = comments;
 
-             result["posts"] = posts;
+            Favorite.scope({method: ["favoritePosts", id]}).all()
+            .then((favorites) => {
+              result["favorites"] = favorites;
 
-             Comment.scope({method: ["lastFiveFor", id]}).all()
-             .then((comments) => {
-               result["comments"] = comments;
-               callback(null, result);
-             })
-             .catch((err) => {
-               callback(err);
-             })
-           })
-         }
-       })
-     }
+              callback(null, result);
+            })
+            .catch((err) => {
+              callback(err);
+            })
+          })
+          .catch((err) => {
+            callback(err);
+          })
+        })
+      }
+    })
+  }
 
 }
